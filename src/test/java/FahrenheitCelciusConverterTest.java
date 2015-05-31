e35577d2b9e7101bc31b01fb3f56fcde3b3209dc
Larry Tambascio <larry@tambascio.net>
2015-05-31 12:34:08 -0400

    Exercise 3.11.4 - Refactored conversion test to use parameterized tests

--- src/test/java/FahrenheitCelciusConverterTest.java
+++ src/test/java/FahrenheitCelciusConverterTest.java
@@ -1,34 +1,61 @@
 import static org.junit.Assert.assertEquals;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Listing 3.17 - Test class to test temperature conversion class.
  * 
  * @author	Larry Tambascio
  */
+@RunWith(JUnitParamsRunner.class)
 public class FahrenheitCelciusConverterTest
 {
+	/**
+	 * Parameter value method for converting Celcius to Fahrenheit
+	 * @return	Test values
+	 */
+	private Object[] toFahrenheitValues()
+	{
+		return new Object[] {
+				new Object[] {32,0},
+				new Object[] {98, 37},
+				new Object[] {212, 100}
+		};
+	}
 
 	/**
 	 * Verify conversion to Fahrenheit
 	 */
 	@Test
-	public void shouldConvertCelciusToFahrneheit()
+	@Parameters(method="toFahrenheitValues")
+	public void shouldConvertCelciusToFahrneheit(int fahrenheit, int celcius)
 	{
-		assertEquals(32, FahrenheitCelciusConverter.toFahrenheit(0));
-		assertEquals(98, FahrenheitCelciusConverter.toFahrenheit(37));
-		assertEquals(212, FahrenheitCelciusConverter.toFahrenheit(100));
+		assertEquals(fahrenheit, FahrenheitCelciusConverter.toFahrenheit(celcius));
+	}
+
+	/**
+	 * Parameter value method for converting Celcius to Fahrenheit
+	 * @return	Test values
+	 */
+	private Object[] toCelciusValues()
+	{
+		return new Object[] {
+				new Object[] {0, 32},
+				new Object[] {37, 100},
+				new Object[] {100, 212}
+		};
 	}
 
 	/**
 	 * Verify conversion to Celcius
 	 */
 	@Test
-	public void shouldConvertFahrenheitToCelcius()
+	@Parameters(method="toCelciusValues")
+	public void shouldConvertFahrenheitToCelcius(int celcius, int fahrenheit)
 	{
-		assertEquals(0, FahrenheitCelciusConverter.toCelcius(32));
-		assertEquals(37, FahrenheitCelciusConverter.toCelcius(100));
-		assertEquals(100, FahrenheitCelciusConverter.toCelcius(212));
+		assertEquals(celcius, FahrenheitCelciusConverter.toCelcius(fahrenheit));
 	}
 }
