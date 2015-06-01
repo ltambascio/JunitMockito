abd53b77727e903bca863482c946e2f18a23fc85
Larry Tambascio <larry@tambascio.net>
2015-06-01 19:07:42 -0400

    4.5.2 - First failure for FootballTeamTest

---
+++ src/main/java/FootballTeam.java
@@ -0,0 +1,13 @@
+
+public class FootballTeam
+{
+	public FootballTeam (int gameswon)
+	{
+		
+	}
+	
+	public int getGamesWon()
+	{
+		return 0;
+	}
+}
---
+++ src/test/java/FootballTeamTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Listing 4.1 - Football Team test class
+ * 
+ * @author Larry Tambascio
+ */
+public class FootballTeamTest
+{
+
+	@Test
+	public void constructorShouldSetGamesWon()
+	{
+		FootballTeam team = new FootballTeam(3);
+		assertEquals(3, team.getGamesWon());
+	}
+
+}
