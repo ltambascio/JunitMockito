aa27c96e807dfdf8d9727f249dbcd9464b79c7e2
Larry Tambascio <larry@tambascio.net>
2015-06-03 16:56:00 -0400

    Refactored compareTo method, as in Listing 4.17.

--- src/main/java/FootballTeam.java
+++ src/main/java/FootballTeam.java
@@ -41,14 +41,6 @@ public class FootballTeam implements Comparable<FootballTeam>
 	@Override
 	public int compareTo(FootballTeam otherTeam)
 	{
-		if (gamesWon > otherTeam.getGamesWon())
-		{
-			return 1;
-		}
-		else if (gamesWon < otherTeam.getGamesWon())
-		{
-			return -1;
-		}
-		return 0;
+		return gamesWon - otherTeam.getGamesWon();
 	}
 }
