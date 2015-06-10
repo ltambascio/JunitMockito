9edfca593bb633545f198a5885d60763c5d8f330
Larry Tambascio <larry@tambascio.net>
2015-06-10 00:00:20 -0400

    Fixed booking system - adjusted data structure to hold Booleans instead of Integers

--- src/main/java/BookingSystem.java
+++ src/main/java/BookingSystem.java
@@ -1,18 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Exercise 4.10.3 - Booking System
+ * 
+ * @author Larry Tambascio
+ */
 public class BookingSystem
 {
+	private ArrayList<Boolean> reservations;
+	
+	/**
+	 * Constructor to initialize reservations array list to have all false, 
+	 * since no reservations have been made yet.
+	 */
+	public BookingSystem()
+	{
+		reservations = new ArrayList<Boolean>(24);
+		for (int i = 0; i < 24; i++)
+		{
+			reservations.add(Boolean.FALSE);
+		}
+	}
 
 	public List<Integer> listBookedHours()
 	{
-		return new ArrayList<Integer>();
+		List<Integer> booked = new ArrayList<Integer>();
+		for (int i = 0; i < 24; i++)
+		{
+			if (reservations.get(i))
+			{
+				booked.add(i);
+			}
+		}
+		return booked;
 	}
 
 	public void reserve(int hour)
 	{
-		// TODO Auto-generated method stub
+		reservations.add(hour, Boolean.TRUE);
 		
 	}
 
