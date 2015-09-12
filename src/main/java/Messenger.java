36ebcdb1890303cdbbd438f46576f6f55f87fcc2
Larry Tambascio <larry@tambascio.net>
2015-09-11 22:52:20 -0400

    Listing 5-12 - Class to be tested with test doubles

---
+++ src/main/java/Messenger.java
@@ -0,0 +1,24 @@
+/**
+ * Listing 5.12 - Messenger class to be tested with test doubles.
+ * 
+ * @author Larry Tambascio
+ */
+public class Messenger
+{
+	private TemplateEngine templateEngine;
+	private MailServer mailServer;
+	
+	public Messenger(MailServer mailServer,
+			TemplateEngine templateEngine)
+	{
+		this.mailServer = mailServer;
+		this.templateEngine = templateEngine;
+	}
+	
+	public void sendMessage(Client client, Template template)
+	{
+		String msgContent = templateEngine.prepareMessage(template, client);
+		
+		mailServer.send(client.getEmail(), msgContent);
+	}
+}
