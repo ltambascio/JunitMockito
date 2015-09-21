d6e664f0937732144424d838e1d6ee34a0e340fb
Larry Tambascio <larry@tambascio.net>
2015-09-20 20:47:45 -0400

    Listing 5.42 - Updated client test to reflect the new constructor

--- src/test/java/testdoubles/ClientTest.java
+++ src/test/java/testdoubles/ClientTest.java
@@ -12,9 +12,8 @@ import org.junit.Test;
  */
 public class ClientTest
 {
-	final static String ANY_NUMBER = "999-888-7777";
-	final static Phone MOBILE_PHONE = new Phone(ANY_NUMBER, true);
-	final static Phone STATIONARY_PHONE = new Phone(ANY_NUMBER, false);
+	final static Phone MOBILE_PHONE = new Phone("+123456789");
+	final static Phone STATIONARY_PHONE = new Phone("+123123123");
 	
 	Client client = new Client();
 
