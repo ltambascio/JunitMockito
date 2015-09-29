312fc0c7f9b2774901a6596a86d5fc7daf25ef1d
Larry Tambascio <larry@tambascio.net>
2015-09-28 23:18:48 -0400

    Enhanced to support categories

--- src/main/java/raceresults/RaceResultsService.java
+++ src/main/java/raceresults/RaceResultsService.java
@@ -1,26 +1,59 @@
 package raceresults;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class RaceResultsService
 {
-	private Collection<Client> clients = new HashSet<Client>();
+	public static final String DEFAULT_CATEGORY = "Default";
+	private Map<String,Collection<Client>> clientCat = new HashMap<String,Collection<Client>>();
+	//private Collection<Client> clients = new HashSet<Client>();
+
+	/**
+	 * Constructor to initialize the default category.
+	 */
+	public RaceResultsService()
+	{
+		clientCat.put(DEFAULT_CATEGORY, new HashSet<Client>());
+	}
 
 	public void addSubscriber(Client client)
 	{
-		this.clients.add(client);
+		clientCat.get(DEFAULT_CATEGORY).add(client);
 	}
 
 	public void send(Message message)
 	{
-		for (Client client : clients)
-			client.receive(message);
+		send(message, DEFAULT_CATEGORY);
 	}
 
 	public void removeSubscriber(Client client)
 	{
-		clients.remove(client);
+		clientCat.get(DEFAULT_CATEGORY).remove(client);
+	}
+
+	public void addSubscriber(Client client, String category)
+	{
+		Collection<Client> clients;
+		
+		if (clientCat.containsKey(category))
+		{
+			clients = clientCat.get(category);
+		}
+		else
+		{
+			clients = new HashSet<Client>();
+			clientCat.put(category, clients);
+		}
+		clients.add(client);
+	}
+	
+	public void send(Message message, String category)
+	{
+		for (Client client : clientCat.get(category))
+			client.receive(message);
 	}
 
 }
